c79ddf09616cebd76365d82f1801dae43e15291d
bes dgv <bes.dgv@gmail.com>
2024-06-13 12:05:31 +0200

    ajout verification userId

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,10 +6,15 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
     const userId = decodedToken.userId;
-    req.auth = {
-      userId: userId,
-    };
-    next();
+    User.findOne({ _id: userId }).then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+      req.auth = {
+        userId: userId,
+      };
+      next();
+    });
   } catch (error) {
     res.status(401).json({ error });
   }
